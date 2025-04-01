c79f5589eb566583cd225174226b73a4f7d1fba4
Yuan <105849859+maybeysz@users.noreply.github.com>
2025-04-01 14:32:43 +0800

    Update glados.py

--- glados.py
+++ glados.py
@@ -37,6 +37,6 @@ if __name__ == '__main__':
             print('cookie已失效')  # 日志输出
     #--------------------------------------------------------------------------------------------------------#   
     if sckey != "":
-        requests.get('http://www.pushplus.plus/send?token=' + sckey + '&title='+email+'签到成功'+'&content='+sendContent)        
+         requests.get('http://www.pushplus.plus/send?token=' + sckey + '&title='+email+'签到成功'+'&content='+sendContent)        
 
 
